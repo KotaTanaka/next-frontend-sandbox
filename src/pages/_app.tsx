191c3f573bac2a19c3b424864afe3e21945974f4
KotaTanaka <te11k.tanaka@gmail.com>
2020-04-15 19:25:00 +0900

    feat: ApolloProviderの設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import { AppProps } from 'next/app';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({ uri: 'http://localhost:3090/graphql' });
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 };
 
 export default App;
