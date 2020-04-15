1bdc0a57b65af29580a1bcac0e93b92c02e82946
KotaTanaka <te11k.tanaka@gmail.com>
2020-04-15 20:00:09 +0900

    fix: fetchを指定しなきゃいけない

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
 import { AppProps } from 'next/app';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
+import fetch from 'isomorphic-fetch';
 
-const client = new ApolloClient({ uri: 'http://localhost:3090/graphql' });
+const client = new ApolloClient({
+  uri: 'http://localhost:3090/graphql',
+  fetch,
+});
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
