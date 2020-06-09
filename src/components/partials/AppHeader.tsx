7d39b3e1be13811bbf4f4f62975dc1174a6a8d83
KotaTanaka <te11k.tanaka@gmail.com>
2020-06-09 19:20:10 +0900

    feat: ヘッダーのメニュー化

--- src/components/partials/AppHeader.tsx
+++ src/components/partials/AppHeader.tsx
@@ -1,5 +1,12 @@
 import { NextComponentType } from 'next';
+import { CSSProperties, useState } from 'react';
 import Link from 'next/link';
+import { Menu, Button } from 'antd';
+import {
+  HomeOutlined,
+  InfoCircleOutlined,
+  MenuOutlined,
+} from '@ant-design/icons';
 import styled from '@emotion/styled';
 
 // from app
@@ -7,14 +14,34 @@ import { PAGE_URL } from '@/constants';
 
 /** ヘッダー */
 const AppHeader: NextComponentType = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+  /** メニューの開閉 */
+  const handleClickMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <Container>
-      <Link href={PAGE_URL.HOME}>
-        <LinkText>Home</LinkText>
-      </Link>
-      <Link href={PAGE_URL.ABOUT}>
-        <LinkText>About Page</LinkText>
-      </Link>
+      <MenuButton type="primary" onClick={handleClickMenu}>
+        <MenuOutlined />
+      </MenuButton>
+      {isMenuOpen && (
+        <Menu theme="dark" style={menuStyle}>
+          <Menu.Item key={PAGE_URL.HOME}>
+            <HomeOutlined />
+            <Link href={PAGE_URL.HOME}>
+              <LinkText>Home</LinkText>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key={PAGE_URL.ABOUT}>
+            <InfoCircleOutlined />
+            <Link href={PAGE_URL.ABOUT}>
+              <LinkText>About Page</LinkText>
+            </Link>
+          </Menu.Item>
+        </Menu>
+      )}
     </Container>
   );
 };
@@ -25,7 +52,18 @@ export default AppHeader;
 const Container = styled.div`
   text-align: center;
 `;
+const MenuButton = styled(Button)`
+  position: absolute;
+  top: 8px;
+  left: 8px;
+`;
 const LinkText = styled.a`
   cursor: pointer;
   margin: 16px;
 `;
+const menuStyle: CSSProperties = {
+  position: 'absolute',
+  top: 44,
+  left: 8,
+  textAlign: 'left',
+};
