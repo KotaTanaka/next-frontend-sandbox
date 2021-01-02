3165825ec11177fae9a79f4b0cb4b5814d6b682c
KotaTanaka <te11k.tanaka@gmail.com>
2021-01-03 01:39:41 +0900

    config: Webpackのエイリアス解決不要になった

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require('path');
-
-module.exports = {
-  webpack(config, options) {
-    config.resolve.alias['@'] = path.join(__dirname, 'src');
-    return config;
-  },
-};
