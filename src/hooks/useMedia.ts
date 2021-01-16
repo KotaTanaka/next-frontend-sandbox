ea6bf81688cb82e0307883097be2d1f9b7859722
KotaTanaka <te11k.tanaka@gmail.com>
2021-01-16 19:37:55 +0900

    feat: メディアクエリフック

---
+++ src/hooks/useMedia.ts
@@ -0,0 +1,16 @@
+/** メディアクエリフック */
+export const useMedia = () => {
+  /** ブレークポイント */
+  const BREAKPOINT = 600;
+
+  /** SPサイズかどうか */
+  const isSP = window.matchMedia(`screen and (max-width: ${BREAKPOINT - 1}px)`);
+
+  /** PCサイズかどうか */
+  const isPC = window.matchMedia(`screen and (min-width: ${BREAKPOINT}px)`);
+
+  return {
+    isSP,
+    isPC,
+  };
+};
