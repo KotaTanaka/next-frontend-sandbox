b9ae11e6d1502dcc4231764c1752c79085f0cc24
KotaTanaka <te11k.tanaka@gmail.com>
2021-09-21 00:24:29 +0900

    feat: 円と年月日変換ユーティリティ

--- src/utils/StringUtils.ts
+++ src/utils/StringUtils.ts
@@ -1,3 +1,4 @@
+import dayjs, { Dayjs } from 'dayjs';
 import { ParsedUrlQuery } from 'querystring';
 
 /**
@@ -9,4 +10,22 @@ export const getIdFromParsedUrlQuery = (query: ParsedUrlQuery): string => {
   const { id } = query;
 
   return Array.isArray(id) ? id[0] : id;
+};
+
+/**
+ * 金額を日本円形式に変換する
+ * @param price 金額
+ * @returns {string} 日本円形式の金額
+ */
+export const toPrice = (price: number) => {
+  return `¥${price.toLocaleString()}`;
+};
+
+/**
+ * 日付を年月日形式に変換する
+ * @param date 任意のフォーマットの日付
+ * @returns {string} 年月日形式の日付
+ */
+export const toJPDate = (date: string | number | Date | Dayjs) => {
+  return dayjs(date).format('YYYY年M月D日');
 };
